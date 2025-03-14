293f8d4917c6298f9ff9c405cc7ab33afc88326a
Yaehoon Kim <eraser3031@gmail.com>
2025-03-14 11:45:28 +0900

    Initial Commit

---
+++ Momento_proto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Momento_proto
+//
+//  Created by 김예훈 on 3/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Momento_proto/Momento_protoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Momento_protoApp.swift
+//  Momento_proto
+//
+//  Created by 김예훈 on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Momento_protoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
